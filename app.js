e7ad39a7f78b798af9a056ff6b733ac751605f5f
thanos324 <tgiobliakis@gmail.com>
2026-03-02 16:12:23 +0200

    new darck  theme

--- app.js
+++ app.js
@@ -565,3 +565,13 @@ document.addEventListener("click", function (e) {
   }
 
 });
+const toggle = document.getElementById("dark-toggle");
+
+toggle.addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
+  localStorage.setItem("darkMode", document.body.classList.contains("dark-mode"));
+});
+
+if (localStorage.getItem("darkMode") === "true") {
+  document.body.classList.add("dark-mode");
+}
